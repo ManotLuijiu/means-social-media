9d33ceb1c60ee5e7c0629393038076ec3a1afe83
Manot Luijiu <manot.luijiu@gmail.com>
2021-03-21 12:05:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
+const winston = require('winston');
+
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+const { MONGODB } = require('./config');
+
+const pubsub = new PubSub();
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'graphql-service' },
+  transport: [
+    // Write all logs with level`error` and below to `error.log`
+    // Write all logs with level `info` and below to `combined.log`
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info(`MongoDb connected.`);
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    logger.log({
+      level: 'info',
+      message: `Server is running at ${res.url}`,
+    });
+  });
