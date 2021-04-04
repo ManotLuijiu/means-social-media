d4b55d6098d2215c0b2d7157e6435024bde6259a
Manot Luijiu <manot.luijiu@gmail.com>
2021-04-04 19:22:46 +0700

    adding dotenv

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -6,7 +6,7 @@ const {
   validateRegisterInput,
   validateLoginInput,
 } = require('../../util/validators');
-const { SECRET_KEY } = require('../../config');
+// const { SECRET_KEY } = require('../../config');
 
 const User = require('../../models/User');
 
@@ -17,7 +17,7 @@ function generateToken(user) {
       email: user.email,
       username: user.username,
     },
-    SECRET_KEY,
+    process.env.SECRET_KEY,
     { expiresIn: '1h' }
   );
 }
