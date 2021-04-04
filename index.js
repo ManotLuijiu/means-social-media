3f06654ef37e7e96249830deeeaf876a8e50b6a9
Manot Luijiu <manot.luijiu@gmail.com>
2021-04-04 18:53:21 +0700

    changing package.json

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { ApolloServer, PubSub } = require('apollo-server');
 const { RedisCache } = require('apollo-server-cache-redis');
 const mongoose = require('mongoose');
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
 
 const resolvers = require('./graphql/resolvers');
 const typeDefs = require('./graphql/typeDefs');
@@ -9,22 +10,28 @@ const { MONGODB } = require('./config');
 
 const pubsub = new PubSub();
 
-const logger = winston.createLogger({
+const PORT = process.env.PORT || 5000;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(label({ label: 'Means Logger' }), timestamp(), myFormat),
   defaultMeta: { service: 'graphql-service' },
   transport: [
     // Write all logs with level`error` and below to `error.log`
     // Write all logs with level `info` and below to `combined.log`
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
   ],
 });
 
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
+    new transports.Console({
+      format: format.simple(),
     })
   );
 }
@@ -53,11 +60,17 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     logger.info(`MongoDb connected.`);
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     logger.log({
       level: 'info',
       message: `Server is running at ${res.url}`,
+    });
+  })
+  .catch((err) => {
+    logger.log({
+      level: 'info',
+      message: `Mongo error ${err}`,
     });
   });
